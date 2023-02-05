7297e4af325a03d655c3dab13f65818256ee9d2d
Sujeet Mainali <mainalisujeet@gmail.com>
2023-02-05 18:00:43 +0545

    addes password hashing and salting to the user model added login and signup routes and pages

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const { isEmail } = require('validator');
+const bcrypt = require('bcrypt')
 
 const userSchema = new mongoose.Schema({
     email : {
@@ -17,6 +18,15 @@ const userSchema = new mongoose.Schema({
 });
 
 
+//use pre method to fire a function before data is saved to database
+userSchema.pre('save', async function(next){
+    // console.log('user about to be created and saved', this);
+    const salt = await bcrypt.genSalt();
+    this.password = await bcrypt.hash(this.password, salt)
+    next();
+})
+
+//use pre method to fire a function after data is saved to database
 userSchema.post('save', function(doc, next){
     console.log(`new user was created and saved${doc}`);
     next();
